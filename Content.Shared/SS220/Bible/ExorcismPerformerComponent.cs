e895a2687a89854e308b3b4b1861584e50017afb
stalengd <p.zamorskiy@yandex.ru>
2024-07-14 01:59:28 +0300

    Added more docs for ExorcismPerformerComponent

--- Content.Shared/SS220/Bible/ExorcismPerformerComponent.cs
+++ Content.Shared/SS220/Bible/ExorcismPerformerComponent.cs
@@ -10,25 +10,49 @@ namespace Content.Shared.SS220.Bible;
 /// </summary>
 public sealed partial class ExorcismPerformerComponent : Component
 {
+    /// <summary>
+    /// Should exorcism be availiable only for creatures with BibleUserComponent.
+    /// </summary>
     [DataField("requiresBibleUser")]
     public bool RequiresBibleUser = true;
 
+    /// <summary>
+    /// Prototype id of an exorcism action.
+    /// </summary>
     [DataField("exorcismAction", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
     public string ExorcismAction = "ActionExorcism";
+    /// <summary>
+    /// Resolved exorcism action entity.
+    /// </summary>
     [DataField("exorcismActionEntity")]
     public EntityUid? ExorcismActionEntity;
 
+    /// <summary>
+    /// Minimum length of a prayer message for exorcism to be available.
+    /// </summary>
     [DataField("messageLengthMin")]
     public int MessageLengthMin = 60;
+    /// <summary>
+    /// Maximum length of a prayer message for exorcism to be available.
+    /// </summary>
     [DataField("messageLengthMax")]
     public int MessageLengthMax = 120;
 
+    /// <summary>
+    /// Range in which exorcism effects will reach other entities.
+    /// </summary>
     [DataField("range")]
     public float Range = 10f;
 
+    /// <summary>
+    /// Duration in seconds of visual light effects.
+    /// </summary>
     [DataField("lightEffectDuration")]
     public float LightEffectDurationSeconds = 1f;
 
+    /// <summary>
+    /// Id of light behaviour to be started on exorcism performance.
+    /// </summary>
     [DataField("lightBehaviourId")]
     public string LightBehaviourId = "exorcism_performance";
 }

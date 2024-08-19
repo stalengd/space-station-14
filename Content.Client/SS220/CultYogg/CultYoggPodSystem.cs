b6b4919c1e937d1b2d501b66cfc13dad7dbb90d7
Gnomeev <137652177+Gnomeev@users.noreply.github.com>
2024-08-19 17:44:06 +0300

    done

---
+++ Content.Client/SS220/CultYogg/CultYoggPodSystem.cs
@@ -0,0 +1,14 @@
+
+using Content.Shared.SS220.CultYogg.Components;
+using Content.Shared.SS220.CultYogg.EntitySystems;
+using Robust.Client.GameObjects;
+
+namespace Content.Client.SS220.CultYogg;
+
+public sealed partial class CultYoggPodSystem : SharedCultYoggPodSystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+}
---
+++ Content.Server/SS220/CultYogg/CultYoggPodSystem.cs
@@ -0,0 +1,83 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
+using Content.Shared.DragDrop;
+using Content.Shared.SS220.CultYogg.Components;
+using Content.Shared.SS220.CultYogg.EntitySystems;
+using Content.Shared.Verbs;
+using Robust.Shared.Containers;
+
+namespace Content.Server.SS220.CultYogg;
+
+public sealed partial class CultYoggPodSystem : SharedCultYoggPodSystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CultYoggPodComponent, EntInsertedIntoContainerMessage>(GotInserted);
+        SubscribeLocalEvent<CultYoggPodComponent, EntRemovedFromContainerMessage>(GotRemoved);
+        SubscribeLocalEvent<CultYoggPodComponent, EntityTerminatingEvent>(GotTerminated);
+        SubscribeLocalEvent<CultYoggPodComponent, DragDropTargetEvent>(OnCanDropHandle);
+        SubscribeLocalEvent<CultYoggPodComponent, GetVerbsEvent<AlternativeVerb>>(AddInsertVerb);
+    }
+
+    private void AddInsertVerb(Entity<CultYoggPodComponent> ent, ref GetVerbsEvent<AlternativeVerb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract)
+            return;
+
+        var target = args.User;
+
+        if (ent.Comp.MobContainer.ContainedEntity is not null)
+        {
+            AlternativeVerb verb = new()
+            {
+                Act = () => TryEject(ent.Comp.MobContainer.ContainedEntity.Value, ent),
+                Category = VerbCategory.Eject,
+                Text = Loc.GetString("cult-yogg-eject-pod"),
+                Priority = 1
+            };
+
+            args.Verbs.Add(verb);
+        }
+
+        if (ent.Comp.MobContainer.ContainedEntity is null)
+        {
+            AlternativeVerb verb = new()
+            {
+                Act = () => TryInsert(target, ent),
+                Category = VerbCategory.Insert,
+                Text = Loc.GetString("cult-yogg-eject-pod"),
+                Priority = 1
+            };
+
+            args.Verbs.Add(verb);
+        }
+    }
+
+    private void OnCanDropHandle(Entity<CultYoggPodComponent> ent, ref DragDropTargetEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = TryInsert(args.Dragged, ent);
+    }
+
+    private void GotTerminated(Entity<CultYoggPodComponent> ent, ref EntityTerminatingEvent args)
+    {
+        if (ent.Comp.MobContainer.ContainedEntity is null)
+            return;
+
+        RemComp<CultYoggHealComponent>(ent.Comp.MobContainer.ContainedEntity.Value);
+    }
+
+    private void GotRemoved(Entity<CultYoggPodComponent> ent, ref EntRemovedFromContainerMessage args)
+    {
+        RemComp<CultYoggHealComponent>(args.Entity);
+    }
+
+    private void GotInserted(Entity<CultYoggPodComponent> ent, ref EntInsertedIntoContainerMessage args)
+    {
+        EnsureComp<CultYoggHealComponent>(args.Entity);
+    }
+}

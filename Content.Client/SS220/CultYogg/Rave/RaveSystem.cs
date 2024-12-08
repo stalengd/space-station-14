21278f41cbd61bd200b33417d5fb5ce59b42f204
SkaldetSkaeg <impotekh@gmail.com>
2024-12-08 19:23:12 +0700

    oculine cleansing rave

--- Content.Client/SS220/CultYogg/Rave/RaveSystem.cs
+++ Content.Client/SS220/CultYogg/Rave/RaveSystem.cs
@@ -1,6 +1,7 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using Content.Client.SS220.TextureFade;
 using Content.Shared.SS220.CultYogg.Rave;
+using Content.Shared.SS220.EntityEffects;
 using Robust.Client.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Player;
@@ -22,6 +23,7 @@ public sealed class RaveSystem : SharedRaveSystem
         SubscribeLocalEvent<RaveComponent, ComponentRemove>(OnRemoved);
         SubscribeLocalEvent<RaveComponent, LocalPlayerAttachedEvent>(OnPlayerAttached);
         SubscribeLocalEvent<RaveComponent, LocalPlayerDetachedEvent>(OnPlayerDetached);
+        SubscribeLocalEvent<RaveComponent, OnChemRemoveHallucinationsEvent>(OnVisionCleansed);
     }
     private void OnAdded(Entity<RaveComponent> entity, ref ComponentAdd args)
     {
@@ -36,7 +38,13 @@ public sealed class RaveSystem : SharedRaveSystem
 
         overlay.IsEnabled = true;
     }
+    private void OnVisionCleansed(Entity<RaveComponent> entity, ref OnChemRemoveHallucinationsEvent args)
+    {
+        if (entity.Comp.EffectEntity is not { } effectEntity)
+            return;
 
+        Del(effectEntity);
+    }
     private void OnRemoved(Entity<RaveComponent> entity, ref ComponentRemove args)
     {
         if (entity.Comp.EffectEntity is not { } effectEntity)

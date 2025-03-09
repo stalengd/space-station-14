7ccf6bac022f0c3c943ae81fd00c72e3c709e9e7
Ady4ik <141335742+Ady4ik@users.noreply.github.com>
2025-03-09 16:07:11 +0300

    FakeMindShield fix (#2673)
    * FakeMS fix
    
    * Fix mindslave
    
    * Update Content.Server/Implants/ImplanterSystem.cs
    
    Co-authored-by: Stalen <33173619+stalengd@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Stalen <33173619+stalengd@users.noreply.github.com>

--- Content.Server/Implants/ImplanterSystem.cs
+++ Content.Server/Implants/ImplanterSystem.cs
@@ -9,6 +9,7 @@ using Content.Shared.Implants.Components;
 using Content.Shared.Interaction;
 using Content.Shared.Mindshield.Components;
 using Content.Shared.Popups;
+using Content.Shared.SS220.MindSlave;
 using Content.Shared.Tag; // SS220-mindslave
 using Robust.Shared.Containers;
 using Robust.Shared.Prototypes;
@@ -30,6 +31,10 @@ public sealed partial class ImplanterSystem : SharedImplanterSystem
     private const string MindShieldImplantTag = "MindShield";
     private const float MindShieldRemoveTime = 40;
     //SS220-mindslave end
+    // SS220-fakeMS fix begin
+    [ValidatePrototypeId<EntityPrototype>]
+    private const string FakeMindShieldImplant = "FakeMindShieldImplant";
+    // SS220-fakeMS fix end
 
     public override void Initialize()
     {
@@ -83,6 +88,21 @@ public sealed partial class ImplanterSystem : SharedImplanterSystem
         }
         //SS220-mindslave end
 
+        // SS220-fakeMSfix begin
+        if (component.Implant == FakeMindShieldImplant)
+        {
+            if (HasComp<MindShieldComponent>(target))
+            {
+                _popup.PopupEntity(Loc.GetString("mindslave-target-mindshielded"), args.User);
+                return;
+            }
+            if (_mindslave.IsEnslaved(target))
+            {
+                _popup.PopupEntity(Loc.GetString("mindshield-target-mindslaved"), target, args.User);
+                return;
+            }
+        }
+        // SS220-fakeMSfix end
         //TODO: Rework when surgery is in for implant cases
         if (component.CurrentMode == ImplanterToggleMode.Draw && !component.ImplantOnly)
         {
